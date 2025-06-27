d255cf850dfde1e312777a53edf6c027dfa34a7f
Dr-Abder <9907@holbertonstudents.com>
2025-06-27 13:57:32 +0000

    Refonte du Projet

--- The_First_Light/V1/backend/__init__.py
+++
@@ -1,4 +0,0 @@
-# backend/__init__.py
-
-# Ce fichier peut rester vide dans un premier temps.
-# Il sert uniquement à indiquer que 'backend' est un package Python.
--- The_First_Light/V1/backend/api.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, request, jsonify
-from backend.core import predict
-from backend.config import DEBUG_MODE
-
-app = Flask(__name__)
