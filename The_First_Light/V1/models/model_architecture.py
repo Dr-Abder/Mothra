f702a63b06c455824e26c3ac3987c4f81dc14724
Dr-Abder <9907@holbertonstudents.com>
2025-07-10 14:14:01 +0200

    Update model_architecture.py

--- The_First_Light/V1/models/model_architecture.py
+++ The_First_Light/V1/models/model_architecture.py
@@ -4,7 +4,7 @@ def build_model():
     model = models.Sequential([
         layers.Input(shape=(128, 128, 3)),              # Entrée : image 128x128 RGB
 
-        layers.Conv2D(32, (3, 3), activation='relu'),   # 1er bloc convolution
+        layers.Conv2D(32, (3, 3), activation='relu'),   # 1er bloc convolution / filtres
         layers.MaxPooling2D(pool_size=(2, 2)),
 
         layers.Conv2D(64, (3, 3), activation='relu'),   # 2e bloc
@@ -13,9 +13,9 @@ def build_model():
         layers.Conv2D(128, (3, 3), activation='relu'),  # 3e bloc
         layers.MaxPooling2D(pool_size=(2, 2)),
 
-        layers.Flatten(),                               # Aplatir pour le Dense
-        layers.Dense(128, activation='relu'),           # Couche dense
-        layers.Dropout(0.5),                            # Pour éviter l’overfitting
+        layers.Flatten(),                               # Données 2D / 1D
+        layers.Dense(128, activation='relu'),           # 128 CO
+        layers.Dropout(0.5),                            # Pour éviter l’overfitting 50ù
 
         layers.Dense(1, activation='sigmoid')           # Sortie binaire : 0 / 1
 ])
