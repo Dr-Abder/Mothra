324c97f2ec2c89044d44019380fb689cf50f472e
Dr-Abder <9907@holbertonstudents.com>
2025-07-18 04:29:26 +0000

    V1 finish

--- The_First_Light/Back-end/app/main.py
+++ The_First_Light/Back-end/app/main.py
@@ -15,7 +15,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://mothra-v1.vercel.app",  # <-- CORRECT
+    "https://mothra.netlify.app",  # <-- CORRECT
 ]
 
 app.add_middleware(
