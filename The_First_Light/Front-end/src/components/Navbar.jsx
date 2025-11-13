208f69dc3f3abe479805aaf23cfa6e8260005ffb
Dr-Abder <9907@holbertonstudents.com>
2025-11-13 13:48:31 +0000

    push

--- The_First_Light/Front-end/src/components/Navbar.jsx
+++ The_First_Light/Front-end/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-beige border-b border-gray-200">
+    <nav className="navbar-bg border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
